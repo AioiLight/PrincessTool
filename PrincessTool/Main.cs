ad2d1a6d3ee38434931586dd9bebf889b3c4ef55
AioiLight <info@aioilight.space>
2020-03-23 03:33:14 +0900

    デフォルトフォルダ

--- PrincessTool/Main.cs
+++ PrincessTool/Main.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,14 @@ namespace PrincessTool
         public Main()
         {
             InitializeComponent();
+
+            var localDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var localLowDir = localDir += "Low";
+            TextBox_Origin_Folder.Text = Path.Combine(localLowDir, @"Cygames\PrincessConnectReDive\");
+
+            var appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            TextBox_Extact_Folder.Text = Path.Combine(appDir, @"dest\");
+
         }
     }
 }
